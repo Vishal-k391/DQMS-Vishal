1ee74e27de89014fe39010b9be801137f33294a5
Vishal-k391 <koriv8335@gmail.com>
2026-04-24 18:31:57 +0530

    few comments added

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import cors from "cors"
 const app = express();
 
 const options_c= {
-    origin : ["http://localhost:5500" , "http://127.0.0.1:5500" , ],
+    origin : ["http://localhost:5500" , "http://127.0.0.1:5500" , ],// added local host for test purposes
     credentials  :true ,
     // methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     // allowedHeaders: ["Content-Type", "Authorization"]
